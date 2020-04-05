2341572ac23ba8fe48e84321c08561c51cd8983a
Paulo Pinheiro <pauloprap57@gmail.com>
2020-04-05 12:33:55 -0300

    Mobile
    Finalizado

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "intl";
+import "intl/locale-data/jsonp/pt-BR";
+import React from "react";
+
+import Routes from "./src/routes";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Ola Mundo</Text>
-    </View>
-  );
+  return <Routes />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#9159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
